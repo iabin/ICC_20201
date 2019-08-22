b777739dde75d01ef193b26ed5d7a6896e4bb69d
Alejandro Iabin Arteaga Hernández <iabin@ciencias.unam.mx>
2019-08-22 02:08:34 -0500

    practica 3 added

---
+++ practica03/HeyJude.java
@@ -0,0 +1,33 @@
+public class HeyJude{
+    String HEY = "Hey Jude ";
+    String DON = "don't ";
+    String MAK = "make it bad.";
+    String BEA = "be afraid.";
+    String LET = "let me down.";
+    String TAK = "Take a sad song and make it better.";
+    String YOW = "You were made to go out and get her.";
+    String YOH = "You have found her, now go and get her.";
+    String REM = "Remember to ";
+    String LEH = "let her into you heart.";
+    String LES = "let her into your skin.";
+    String THE = "Then you ";
+    String CAN = "can start ";
+    String BEG = "begin ";
+    String TOM = "to make it better ";
+    String BET = "better ";
+    String NA  = "na ";
+
+    String AND = "And anytime you feel the pain, hey Jude, refrain,\n"
+	                            + "Don’t carry the world upon your shoulders.\n"
+                                    + "For well you know that it’s a fool who plays it cool\n"
+	                            + "By making his world a little colder.";
+
+    String SOL = "So let it out and let it in, hey Jude, begin,\n"
+	                            + "You’re waiting for someone to perform with.\n"
+	                            + "And don’t you know that it’s just you, hey jude, you’ll do,\n"
+	                            + "The movement you need is on your shoulder.";
+
+    public static void main(String[] args){
+        
+    }   
+}
