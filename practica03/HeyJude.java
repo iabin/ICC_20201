d462812a889175ac4d8b4fbe5ac91a191db88cc0
Alejandro Iabin Arteaga Hernández <iabin@ciencias.unam.mx>
2019-08-22 11:35:47 -0500

    arreglando practica3

--- practica03/HeyJude.java
+++ practica03/HeyJude.java
@@ -1,28 +1,28 @@
 public class HeyJude{
-    String HEY = "Hey Jude ";
-    String DON = "don't ";
-    String MAK = "make it bad.";
-    String BEA = "be afraid.";
-    String LET = "let me down.";
-    String TAK = "Take a sad song and make it better.";
-    String YOW = "You were made to go out and get her.";
-    String YOH = "You have found her, now go and get her.";
-    String REM = "Remember to ";
-    String LEH = "let her into you heart.";
-    String LES = "let her into your skin.";
-    String THE = "Then you ";
-    String CAN = "can start ";
-    String BEG = "begin ";
-    String TOM = "to make it better ";
-    String BET = "better ";
-    String NA  = "na ";
+    public static String HEY = "Hey Jude ";
+    public static String DON = "don't ";
+    public static String MAK = "make it bad.";
+    public static String BEA = "be afraid.";
+    public static String LET = "let me down.";
+    public static String TAK = "Take a sad song and make it better.";
+    public static String YOW = "You were made to go out and get her.";
+    public static String YOH = "You have found her, now go and get her.";
+    public static String REM = "Remember to ";
+    public static String LEH = "let her into you heart.";
+    public static String LES = "let her into your skin.";
+    public static String THE = "Then you ";
+    public static String CAN = "can start ";
+    public static String BEG = "begin ";
+    public static String TOM = "to make it better ";
+    public static String BET = "better ";
+    public static String NA  = "na ";
 
-    String AND = "And anytime you feel the pain, hey Jude, refrain,\n"
+    public static String AND = "And anytime you feel the pain, hey Jude, refrain,\n"
 	                            + "Don’t carry the world upon your shoulders.\n"
                                     + "For well you know that it’s a fool who plays it cool\n"
 	                            + "By making his world a little colder.";
 
-    String SOL = "So let it out and let it in, hey Jude, begin,\n"
+    public static String SOL = "So let it out and let it in, hey Jude, begin,\n"
 	                            + "You’re waiting for someone to perform with.\n"
 	                            + "And don’t you know that it’s just you, hey jude, you’ll do,\n"
 	                            + "The movement you need is on your shoulder.";
