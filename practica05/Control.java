95b61168a4a0ac6559895c6253bd0b45bce5ffe4
Iabin Arteaga <iabin@ciencias.unam.mx>
2019-10-03 11:57:23 -0500

    agregando practica 5

---
+++ practica05/Control.java
@@ -0,0 +1,33 @@
+public class Control{
+
+    
+    public static boolean problema1(String palabra){
+        return true;
+    }
+    
+    public static boolean problema2(int[] arreglo, int k){
+        return true;
+    }
+    
+    public static void problema3(int[] arreglo){
+        return;
+    }
+    
+    public static boolean problema4(int[] arreglo, int buscando){
+        return true;
+    }
+
+    public static boolean problema5(int n){
+        return true;
+    }
+    
+    public static int[] problema6(int n){
+        return new int[10];
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+
+}
