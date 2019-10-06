965d2ad9d915a1309620c0362ca8276a17118fd6
Iabin Arteaga <iabin@ciencias.unam.mx>
2019-10-05 22:09:04 -0500

    agregando link al código

--- practica05/Control.java
+++ practica05/Control.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Control{
 
     
@@ -10,6 +13,7 @@ public class Control{
     }
     
     public static void problema3(int[] arreglo){
+        Arrays.sort(arreglo);
         return;
     }
     
@@ -29,17 +33,19 @@ public class Control{
     public static void main(String[] args) {
         String resultado = "";
         int[] desordenado = {9,8,7,6,5,4,3,2,1};
-        int[] ordenado = {1,2,3,4,5,6,7,8,9};
+        int[] ordenado    = {1,2,3,4,5,6,7,8,9};
         int[] primos10 = {2,3,5,7};
         if(problema1("oso")==true && problema1("palabra")==false)resultado+="P1 bien\n";else resultado+="P1 mal\n";
         if(problema2(desordenado, 17)==true) resultado+="P2 bien\n";else resultado+="P2 mal\n";
+        System.out.println(Arrays.toString(desordenado));
         problema3(desordenado);
-        if(desordenado.equals(ordenado) == true)resultado+="P3 bien\n";else resultado+="P3 mal\n";
+        System.out.println(Arrays.toString(desordenado));
+        System.out.println(desordenado.equals(ordenado));
+        if(Arrays.equals(desordenado,ordenado) == true)resultado+="P3 bien\n";else resultado+="P3 mal\n";
         if(problema4(desordenado,2)== true && problema4(desordenado,18)== false)resultado+="P4 bien\n";else resultado+="P4 mal\n";
         if(problema5(7)==true && problema5(6)==false)resultado+="P5 bien\n";else resultado+="P5 mal\n";
-        if(problema6(10).equals(primos10) == true)resultado+="P6 bien\n";else resultado+="P6 mal\n";
-        System.out.println(resultado);
-        
+        if(Arrays.equals(problema6(10), primos10) == true)resultado+="P6 bien\n";else resultado+="P6 mal\n";
+        System.out.println(resultado);   
     }
 
 }
